40ff69553eb5c134d536cc2739040fd0031e8e6b
GyuDev <yrtnm1@gmail.com>
2019-11-17 15:27:32 +0900

    Bullet 발사 추가

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	private float bulletSpeed = 500f;
+	private Rigidbody2D bulletRigidbody;
+
+	private void Start()
+	{
+		bulletRigidbody = GetComponent<Rigidbody2D>();
+		bulletRigidbody.AddForce(new Vector2(bulletSpeed, 0));
+
+		Destroy(gameObject, 2f);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Monster")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerStauts.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStauts : MonoBehaviour
+{
+	public GameObject bulletPrefab;
+	//private GameObject player;
+
+	private float timeSpawnBullet;
+	public float spawnBullet = 1f;
+	private Transform targetMonster;
+
+	private void Awake()
+	{
+		targetMonster = FindObjectOfType<MonsterStatus>().transform;
+	}
+	private void Start()
+	{
+		
+	}
+
+	private void Update()
+	{
+		
+	}
+
+	private void FixedUpdate()
+	{
+		timeSpawnBullet += Time.deltaTime;
+		if(spawnBullet <= timeSpawnBullet)
+		{
+			timeSpawnBullet = 0f;
+			BulletFire();
+		}
+	}
+
+	private void PlayerDie()
+	{
+
+	}
+
+	private void BulletFire()
+	{
+		GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+		
+	}
+}
