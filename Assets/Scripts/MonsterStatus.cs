94fdb3072c75eeb4b3793ac7ab67a77ae14e64e9
Gyudev <yrtnm1@gmail.com>
2019-11-23 14:51:43 +0900

    몬스터 공격 추가

--- Assets/Scripts/MonsterStatus.cs
+++ Assets/Scripts/MonsterStatus.cs
@@ -1,18 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MonsterStatus : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	public PlayerStauts playerStauts;
+
+	public GameObject monster;
+	private Rigidbody2D monsterRigid;
+
+	public GameObject bulletPrefab;
+
+	public Image monsterHpBar;
+
+	private float timeSpawnBullet;
+	public float spawnBullet = 2f;
+
+	public float monsterHp { get; set; }
+	public float monsterDamage { get; set; }
+
+	private void Start()
+    {
+		monsterRigid = GetComponent<Rigidbody2D>();
+		monsterHp = 5f;
+		monsterDamage = 10f;
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void FixedUpdate()
+	{
+		timeSpawnBullet += Time.deltaTime;
+		if(spawnBullet <= timeSpawnBullet)
+		{
+			timeSpawnBullet = 0;
+			Attack();
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.tag == "Bullet")
+		{
+			monsterHpBar.fillAmount -= playerStauts.playerDamage / monsterHp;
+
+			if (monsterHpBar.fillAmount <= 0.1f)
+			{
+				MonsterDie();
+			}
+		}
+	}
+
+	private void MonsterDie()
+	{
+		monsterRigid.AddForce(new Vector2(300f, 100f));
+		Destroy(monster, 1f);
+	}
+
+	private void Attack()
+	{
+		GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+	}
 }
--- Assets/Scripts/PlayerStauts.cs
+++ Assets/Scripts/PlayerStauts.cs
@@ -1,23 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStauts : MonoBehaviour
 {
+	public MonsterStatus monsterStatus;
+
 	public GameObject bulletPrefab;
-	//private GameObject player;
+	private Animator dogAnim;
+	public Image playerHpBar;
+	public Rigidbody2D playerRigid;
+
 
 	private float timeSpawnBullet;
 	public float spawnBullet = 1f;
-	private Transform targetMonster;
+
+	public float playerHp	{ get; set;	}
+	public float playerDamage { get; set; }
 
 	private void Awake()
 	{
-		targetMonster = FindObjectOfType<MonsterStatus>().transform;
+		dogAnim = GetComponent<Animator>();
 	}
+
 	private void Start()
 	{
-		
+		playerHp = 10f;
+		playerDamage = 1f;
 	}
 
 	private void Update()
@@ -31,18 +41,31 @@ public class PlayerStauts : MonoBehaviour
 		if(spawnBullet <= timeSpawnBullet)
 		{
 			timeSpawnBullet = 0f;
-			BulletFire();
+			Attck();
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.tag == "MonsterBullet")
+		{
+			playerHpBar.fillAmount -= monsterStatus.monsterDamage / playerHp;
+			if (playerHpBar.fillAmount <= 0.1f)
+			{
+				PlayerDie();
+			}
 		}
 	}
 
 	private void PlayerDie()
 	{
-
+		playerRigid.AddForce(new Vector2(0f, 200f));
+		dogAnim.SetInteger("intDie", 0);
 	}
 
-	private void BulletFire()
+	private void Attck()
 	{
+		dogAnim.SetTrigger("doAttack");
 		GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
-		
 	}
 }
