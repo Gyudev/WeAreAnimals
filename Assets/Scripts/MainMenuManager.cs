d7aa791fe1ab4359f4eaf66f0c6946ef1a2f7c25
Hoeya <53281767+Gyudev@users.noreply.github.com>
2020-01-03 17:34:06 +0900

    게임시작 버튼 활성화

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+	public Transform stagePanel;
+	public Button gameStartButton;
+
+	private bool isStagePanel = false;
+
+	public void StageButtonClick()
+	{
+		if (!isStagePanel)
+		{
+			stagePanel.gameObject.SetActive(true);
+		}
+	}
+}
